e3414744c93428210cfdb7363fd784b7d9132aae
KhlystovArtem <test@mail.yyy>
2021-07-02 18:31:11 +0300

    Добавили основную структуру приложения

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+import pygame
+
+
+def run_game():
+    pygame.init()
+    pygame.display.set_caption("Alien Invasion")
+
+    while True:
+        pass
+
+
+if __name__ == '__main__':
+    run_game()
